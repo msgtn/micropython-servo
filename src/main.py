fdd16fe34827bff92e176e61a447a00e616cffe7
Manuel Widmer <mawidme@gmail.com>
2025-08-26 16:59:04 +0200

    added LED test

--- src/main.py
+++ src/main.py
@@ -21,6 +21,12 @@ robot = Robot(motor_pin_ids={1: 18, 2: 19, 3: 20, 4: 21})
 
 uart = UART(1, 115200, tx=Pin(4), rx=Pin(5), timeout_char=100)
 
+# LED test: blink twice
+for _ in range(2):
+    led.value(1)
+    time.sleep(0.3)
+    led.value(0)
+    time.sleep(0.3)
 
 def cmddict_from_msg(msg: str):
     # input like: 1=50&2=30
